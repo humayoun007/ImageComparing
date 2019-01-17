46d85baaa8129a493caebdfff98fd9417b267654
Md. Humayoun Kabir <humayounkabir30@gmail.com>
2019-01-17 17:30:37 +0600

    Add files via upload

---
+++ SampleImageComparing/Program.cs
@@ -0,0 +1,33 @@
+﻿using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleImageCompairing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            using (Py.GIL())
+            {
+                dynamic ci = Py.Import("compare_images");
+                dynamic compare_image = ci.CompareImage("images/messi_image.jpg", "images2/horse-animal.jpg");
+                dynamic image_difference = compare_image.compare_image();
+                String isMatchedObj =  Convert.ToString(image_difference);
+
+                if (isMatchedObj.ToString().Equals("Matched"))
+                {
+                    Console.WriteLine("Matched");
+                }
+
+                Console.ReadKey();
+
+                
+
+            }
+        }
+    }
+}
